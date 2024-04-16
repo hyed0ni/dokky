8b368331a19a864902d9fa1b692c0941fc33bb47
drk219 <drk219@naver.com>
2024-04-16 14:25:09 +0900

    KDR

--- src/main/java/com/mcp/semi/user/mapper/UserMapper.java
+++ src/main/java/com/mcp/semi/user/mapper/UserMapper.java
@@ -1,4 +1,14 @@
 package com.mcp.semi.user.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.mcp.semi.user.dto.UserDto;
+
+@Mapper
 public interface UserMapper {
+
+	void setSignin(UserDto user);
+	void setSignup(UserDto user);
+	void setSignout(UserDto user);
+	
 }
