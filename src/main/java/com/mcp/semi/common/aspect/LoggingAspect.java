d0253dc2319073a03712c6869bd6f48d83c68f5c
Baemint2 <qofndlwl@gmail.com>
2024-04-17 17:11:05 +0900

    AOP 추가

---
+++ src/main/java/com/mcp/semi/common/aspect/LoggingAspect.java
@@ -0,0 +1,36 @@
+package com.mcp.semi.common.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    @Around("execution(* com.mcp.semi..*.*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+
+        String simpleClassName = className.substring(className.lastIndexOf('.') + 1);
+
+        log.info("메소드 시작 : {}.{}", simpleClassName, methodName);
+        // 메소드 실행 전 시간 측정 
+        long start = System.currentTimeMillis();
+        
+
+        try {
+            // 대상 메소드 실행
+            return joinPoint.proceed();
+        } finally {
+            // 메소드 실행 후 시간 측정 및 실행 시간 계산
+            long executionTime = System.currentTimeMillis() - start;
+            log.info("메소드 종료 : {}.{} 실행 시간: {} ms",simpleClassName, methodName ,executionTime);
+        }
+    }
+}
