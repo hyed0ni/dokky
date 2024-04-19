24fa1510b23574a88a512acbdcac0626227074f5
Baemint2 <qofndlwl@gmail.com>
2024-04-19 09:16:44 +0900

    사용자 정보 및 내가 작성한글 조회 구현

--- src/main/java/com/mcp/semi/mypage/controller/MyPageController.java
+++ src/main/java/com/mcp/semi/mypage/controller/MyPageController.java
@@ -1,18 +1,34 @@
 package com.mcp.semi.mypage.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mcp.semi.board.dto.BoardDto;
+import com.mcp.semi.mypage.service.MyPageService;
+import com.mcp.semi.user.dto.UserDto;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
 @RequestMapping("mypage")
+@RequiredArgsConstructor
 public class MyPageController {
 	
-	@GetMapping("")
-	public String myProfile() {
+	private final MyPageService myPageService;
+	
+	@GetMapping("{userNo}")
+	public String myProfile(@PathVariable("userNo") int userNo, Model model) {
+		UserDto userProfile = myPageService.getUserProfile(userNo);
+		model.addAttribute("user",userProfile);
 		return "mypage/myProfile";
 	}
 	
@@ -25,5 +41,16 @@ public class MyPageController {
 	public String removeConfirm() {
 		return "mypage/removeConfirm";
 	}
+	
+	@GetMapping(value="api/myBoard", produces = "application/json")
+	public ResponseEntity<?> myBoard() {
+		int userId = 1;
+		List<BoardDto> boardList = myPageService.getUserPosts(userId);
+		if(boardList.isEmpty()) {
+			return ResponseEntity.ok(Map.of("message", "아직 작성한 게시글이 없습니다"));
+		}
+		return ResponseEntity.ok(boardList);
+	}
+
 
 }
--- src/main/java/com/mcp/semi/mypage/mapper/MyPageMapper.java
+++ src/main/java/com/mcp/semi/mypage/mapper/MyPageMapper.java
@@ -1,5 +1,13 @@
 package com.mcp.semi.mypage.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.mcp.semi.board.dto.BoardDto;
+
+@Mapper
 public interface MyPageMapper {
 
+	List<BoardDto> getPostsByUserId(int userNo);
 }
