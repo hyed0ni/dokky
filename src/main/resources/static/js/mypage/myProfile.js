faaefc30dc7f57e20317c40aaeadf0e7e292f2c1
Baemint2 <qofndlwl@gmail.com>
2024-04-22 07:24:19 +0900

    쌍따옴표로 변경

--- src/main/resources/static/js/mypage/myProfile.js
+++ src/main/resources/static/js/mypage/myProfile.js
@@ -1,99 +1,99 @@
 let originalProfileContent = null;
 
-document.addEventListener('DOMContentLoaded', function() {
-	const profileContainer = document.getElementById('profile-container');
-	originalProfileContent = document.createElement('div');
+document.addEventListener("DOMContentLoaded", function() {
+	const profileContainer = document.getElementById("profile-container");
+	originalProfileContent = document.createElement("div");
 	Array.from(profileContainer.children).forEach(child => {
 		originalProfileContent.appendChild(child.cloneNode(true));
 	});
 
-	document.getElementById('update-pw-btn').addEventListener('click', () => {
-		location.href = '/dokky/modify-password';
-	})
+	document.getElementById("update-pw-btn").addEventListener("click", () => {
+		location.href = "/dokky/modify-password";
+	});
 
-	document.getElementById("remove-user-btn").addEventListener('click', () => {
-		location.href = '/dokky/remove-user';
-	})
+	document.getElementById("remove-user-btn").addEventListener("click", () => {
+		location.href = "/dokky/remove-user";
+	});
 
-	document.getElementById('my-activity').addEventListener('click', function(event) {
+	document.getElementById("my-activity").addEventListener("click", function(event) {
 		event.preventDefault();
 		removeActiveClass();
-		this.classList.add('active');
+		this.classList.add("active");
 		showActivityRecords();
 	});
 
-	const profileLink = document.getElementById('profile-link');
-	profileLink.addEventListener('click', function(event) {
+	const profileLink = document.getElementById("profile-link");
+	profileLink.addEventListener("click", function(event) {
 		event.preventDefault();
 		removeActiveClass();
-		this.classList.add('active');
+		this.classList.add("active");
 		showProfile();
 	});
 });
 
 function removeActiveClass() {
-	document.querySelectorAll('.nav-link').forEach(function(link) {
-		link.classList.remove('active');
+	document.querySelectorAll(".nav-link").forEach(function(link) {
+		link.classList.remove("active");
 	});
 }
 
 function showActivityRecords() {
-	clearContent('profile-container');
-	const profileContainer = document.getElementById('profile-container');
-	profileContainer.textContent = '';
+	clearContent("profile-container");
+	const profileContainer = document.getElementById("profile-container");
+	profileContainer.textContent = "";
 
 	// 활동기록 컨테이너 
-	const activityRecords = document.createElement('div');
-	activityRecords.id = 'activity-records';
+	const activityRecords = document.createElement("div");
+	activityRecords.id = "activity-records";
 
 	//제목
-	const header = document.createElement('h4');
-	header.classList.add('mb-3');
-	header.textContent = '활동기록';
+	const header = document.createElement("h4");
+	header.classList.add("mb-3");
+	header.textContent = "활동기록";
 	activityRecords.appendChild(header);
 
 	//프로필 및 컨테이너
-	const myProfileBorder = document.createElement('div');
-	myProfileBorder.className = 'my-profile-border';
+	const myProfileBorder = document.createElement("div");
+	myProfileBorder.className = "my-profile-border";
 
-	const uiBox = document.createElement('div');
-	uiBox.className = 'ui-box';
+	const uiBox = document.createElement("div");
+	uiBox.className = "ui-box";
 	// 프로필 UI
-	const myProfileUI = document.createElement('div');
-	myProfileUI.className = 'my-profile-ui'
+	const myProfileUI = document.createElement("div");
+	myProfileUI.className = "my-profile-ui";
 
-	const profileImage = document.createElement('img');
-	profileImage.src = '/images/profileImage.jpg'
-	profileImage.className = 'profile-img me-4';
+	const profileImage = document.createElement("img");
+	profileImage.src = "/images/profileImage.jpg";
+	profileImage.className = "profile-img me-4";
 
-	const span = document.createElement('span');
-	span.textContent = '모지모지';
+	const span = document.createElement("span");
+	span.textContent = "모지모지";
 	myProfileUI.appendChild(profileImage);
 	myProfileUI.appendChild(span);
 	uiBox.appendChild(myProfileUI);
 	myProfileBorder.appendChild(uiBox);
 
-	const activityTabs = document.createElement('div');
-	activityTabs.className = 'activity-tabs'
+	const activityTabs = document.createElement("div");
+	activityTabs.className = "activity-tabs"
 
 	//작성한 글
-	const boardsLink = document.createElement('a');
-	boardsLink.htef = '#';
-	boardsLink.id = 'my-boards-button';
-	boardsLink.className = 'tab-link';
+	const boardsLink = document.createElement("a");
+	boardsLink.htef = "#";
+	boardsLink.id = "my-boards-button";
+	boardsLink.className = "tab-link";
 
-	const boardsLinkSpan = document.createElement('span');
-	boardsLinkSpan.textContent = '작성한 글';
+	const boardsLinkSpan = document.createElement("span");
+	boardsLinkSpan.textContent = "작성한 글";
 	boardsLink.appendChild(boardsLinkSpan);
 
 	//댓글
-	const commentsLink = document.createElement('a');
-	commentsLink.htef = '#';
-	commentsLink.id = 'my-comments-button';
-	commentsLink.className = 'tab-link';
+	const commentsLink = document.createElement("a");
+	commentsLink.htef = "#";
+	commentsLink.id = "my-comments-button";
+	commentsLink.className = "tab-link";
 
-	const commentsLinkSpan = document.createElement('span');
-	commentsLinkSpan.textContent = '댓글';
+	const commentsLinkSpan = document.createElement("span");
+	commentsLinkSpan.textContent = "댓글";
 	commentsLink.appendChild(commentsLinkSpan);
 
 
@@ -102,84 +102,84 @@ function showActivityRecords() {
 
 
 	//컨텐츠 영역
-	const activityContent = document.createElement('div');
-	activityContent.id = 'activity-content';
+	const activityContent = document.createElement("div");
+	activityContent.id = "activity-content";
 
 	myProfileBorder.appendChild(activityTabs);
 	myProfileBorder.appendChild(activityContent);
 
 	activityRecords.appendChild(myProfileBorder);
 	profileContainer.appendChild(activityRecords);
-	setupActivityTabs()
+	setupActivityTabs();
 }
 
 function setupActivityTabs() {
-	document.querySelectorAll('.tab-link').forEach(tab => {
-		tab.addEventListener('click', function(e) {
+	document.querySelectorAll(".tab-link").forEach(tab => {
+		tab.addEventListener("click", function(e) {
 			e.preventDefault();
-			const tabs = document.querySelectorAll('.tab-link');
-			tabs.forEach(tab => tab.classList.remove('active'));
-			tab.classList.add('active');
-			if (tab.id === 'my-boards-button') {
+			const tabs = document.querySelectorAll(".tab-link");
+			tabs.forEach(tab => tab.classList.remove("active"));
+			tab.classList.add("active");
+			if (tab.id === "my-boards-button") {
 				showBoards();
-			} else if (tab.id === 'my-comments-button') {
+			} else if (tab.id === "my-comments-button") {
 				showComments();
-			}
-		})
-	})
+			};
+		});
+	});
 
 }
 
 
 function showContent(apiUrl, render) {
-	clearContent('activity-content');
-	const userNo = document.getElementById('user-no').value;
+	clearContent("activity-content");
+	const userNo = document.getElementById("user-no").value;
 	const compleUrl = apiUrl + userNo;
 	fetch(compleUrl)
 		.then(response => response.json())
 		.then(resData => {
-			const activityContent = document.getElementById('activity-content');
+			const activityContent = document.getElementById("activity-content");
 			if (resData.length > 0) {
 				activityContent.appendChild(renderUserBoards(resData));
 			} else {
 				activityContent.textContent = resData.message;
-			}
+			};
 		})
 		.catch(error => {
-			console.error('Error fetching user posts:', error);
+			console.error("Error fetching user posts:", error);
 		});
 }
 
 function showComments() {
-	showContent('/dokky/api/my-comment/', renderBoardRepeatedWithComments);
+	showContent("/dokky/api/my-comment/", renderBoardRepeatedWithComments);
 }
 
 function showBoards() {
-	showContent('/dokky/api/my-board/', renderUserBoards);
+	showContent("/dokky/api/my-board/", renderUserBoards);
 }
 
 function createActivityCard({ boardNo, boardTitle, content, date, link, contentLength = 20 }) {
-	const activityCard = document.createElement('div');
-	activityCard.className = 'activity-card';
+	const activityCard = document.createElement("div");
+	activityCard.className = "activity-card";
 
 	// 제목
-	const activityTitle = document.createElement('div');
-	activityTitle.className = 'activity-title';
+	const activityTitle = document.createElement("div");
+	activityTitle.className = "activity-title";
 
-	const activityLink = document.createElement('a');
+	const activityLink = document.createElement("a");
 	activityLink.href = link;
 	activityLink.textContent = boardTitle;
 
 	// 등록일
-	const activityDate = document.createElement('small');
-	activityDate.className = 'activity-date';
+	const activityDate = document.createElement("small");
+	activityDate.className = "activity-date";
 	activityDate.textContent = new Date(date).toLocaleString();
 
 	activityTitle.appendChild(activityLink);
 	activityTitle.appendChild(activityDate);
 
-	const activityContent = document.createElement('p');
-	activityContent.className = 'activity-content';
+	const activityContent = document.createElement("p");
+	activityContent.className = "activity-content";
 	activityContent.textContent = content.length > contentLength ? `${content.substring(0, contentLength)}...` : content;
 
 	activityCard.appendChild(activityTitle);
@@ -190,8 +190,8 @@ function createActivityCard({ boardNo, boardTitle, content, date, link, contentL
 
 function renderUserBoards(resData) {
 	// 게시물 목록 컨테이너
-	const postsContainer = document.createElement('div');
-	postsContainer.className = 'activity-container';
+	const postsContainer = document.createElement("div");
+	postsContainer.className = "activity-container";
 
 	resData.forEach(board => {
 		const postCard = createActivityCard({
@@ -200,7 +200,7 @@ function renderUserBoards(resData) {
 			content: board.boardContent,
 			date: board.createDt,
 			link: `/dokky/detail?boardNo=${board.boardNo}`
-		})
+		});
 		postsContainer.appendChild(postCard);
 	});
 
@@ -211,8 +211,8 @@ function renderUserBoards(resData) {
 function renderBoardRepeatedWithComments(resData) {
 
 	// 게시물 목록 컨테이너
-	const commentsContainer = document.createElement('div');
-	commentsContainer.className = 'activity-container';
+	const commentsContainer = document.createElement("div");
+	commentsContainer.className = "activity-container";
 
 	resData.forEach(board => {
 		board.comments.forEach(comment => {
@@ -232,10 +232,10 @@ function renderBoardRepeatedWithComments(resData) {
 
 
 function showProfile() {
-	const profileContainer = document.getElementById('profile-container');
+	const profileContainer = document.getElementById("profile-container");
 	while (profileContainer.firstChild) {
 		profileContainer.removeChild(profileContainer.firstChild);
-	}
+	};
 
 	Array.from(originalProfileContent.children).forEach(child => {
 		profileContainer.appendChild(child.cloneNode(true));
@@ -247,15 +247,14 @@ function clearContent(content) {
 	const activityContent = document.getElementById(content);
 	while (activityContent.firstChild) {
 		activityContent.removeChild(activityContent.firstChild);
-	}
+	};
 }
 
-
 function rebindEventListeners() {
-	document.getElementById('update-pw-btn').addEventListener('click', () => {
-		location.href = '/dokky/modify-password';
+	document.getElementById("update-pw-btn").addEventListener("click", () => {
+		location.href = "/dokky/modify-password";
 	});
-	document.getElementById("remove-user-btn").addEventListener('click', () => {
-		location.href = '/dokky/remove-user';
+	document.getElementById("remove-user-btn").addEventListener("click", () => {
+		location.href = "/dokky/remove-user";
 	});
 }
