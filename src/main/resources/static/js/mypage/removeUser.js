07ddcb9f733e2a954f452a58b1d51b6a872eddf0
hyed0ni <layssing10955@gmail.com>
2024-04-27 18:49:43 +0900

    회원 탈퇴 취소 경로 수정

--- src/main/resources/static/js/mypage/removeUser.js
+++ src/main/resources/static/js/mypage/removeUser.js
@@ -4,7 +4,7 @@ const removeBtn = document.getElementById("remove-user-btn");
 
 // 취소 버튼 클릭 시 마이페이지로 이동
 cancelBtn.addEventListener("click", () => 
-    location.href = "/dokky/mypage/1");
+    location.href = "/dokky/mypage");
 
 // 비밀번호 입력 시 탈퇴 버튼 활성화
 document.getElementById("origin-pw").addEventListener("input", e => {
