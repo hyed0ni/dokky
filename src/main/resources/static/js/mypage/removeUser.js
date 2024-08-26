6c5266b47d2207e507e4e61a6964d3357adc21b2
WOO HYEJIN <layssing10955@gmail.com>
2024-08-26 23:53:47 +0900

    removeUser.js 제이쿼리로 변경

--- src/main/resources/static/js/mypage/removeUser.js
+++ src/main/resources/static/js/mypage/removeUser.js
@@ -1,13 +1,11 @@
 // 취소, 탈퇴 버튼
-const cancelBtn = document.getElementById("cancel-btn");
-const removeBtn = document.getElementById("remove-user-btn");
+const $cancelBtn = $("#cancel-btn");
+const $removeBtn = $("#remove-user-btn");
 
 // 취소 버튼 클릭 시 마이페이지로 이동
-cancelBtn.addEventListener("click", () => 
-    location.href = "/dokky/mypage");
+$cancelBtn.on("click", () => location.href = "/dokky/mypage");
 
 // 비밀번호 입력 시 탈퇴 버튼 활성화
-document.getElementById("origin-pw").addEventListener("input", e => {
-    if (e.target.value.length >= 4) removeBtn.classList.remove("disabled");
-    else removeBtn.classList.add("disabled");
+$("#origin-pw").on("input", e => {
+    $removeBtn.toggleClass("disabled", $(e.target).val().length < 4);
 });
