4885331409ef1ca729b7a071b9258ab47295c1b3
WOO HYEJIN <layssing10955@gmail.com>
2024-08-28 23:14:20 +0900

    modifyPw.js 제이쿼리로 변경 + 신규 비밀번호 확인 오류 수정

--- src/main/resources/static/js/mypage/modifyPw.js
+++ src/main/resources/static/js/mypage/modifyPw.js
@@ -1,91 +1,92 @@
 // 비밀번호 입력 input
-const originPw = document.getElementById("origin-pw");
-const newPw = document.getElementById("new-pw");
-const newPwChk = document.getElementById("new-pw-chk");
+const $originPw = $("#origin-pw");
+const $newPw = $("#new-pw");
+const $newPwChk = $("#new-pw-chk");
 
 // 검증 메시지
-const originPwMsg = document.getElementById("origin-pw-msg");
-const newPwMsg = document.getElementById("new-pw-msg");
-const newPwChkMsg = document.getElementById("new-pw-chk-msg");
+const $originPwMsg = $("#origin-pw-msg");
+const $newPwMsg = $("#new-pw-msg");
+const $newPwChkMsg = $("#new-pw-chk-msg");
 
 // 비밀번호 변경 form
-const modifyForm = document.getElementById("modify-form");
+const $modifyForm = $("#modify-form");
 
 // 현재 비밀번호
-originPw.addEventListener("blur", () => 
-	originPwMsg.className = !empInput(originPw) ? "none" : "block");
+$originPw.on("blur", () => {
+    const isEmpty = empInput($originPw);
+    $originPwMsg.toggleClass("block", isEmpty).toggleClass("none", !isEmpty);
+});
 
 // 신규 비밀번호 유효성 검사
-newPw.addEventListener("change", e => {
+$newPw.on("change", e => {
+	
+    const newPwVal = $(e.target).val();
 
-    if (empInput(e.target)) {
-        newPwMsg.className = "none";
+    if (empInput($(e.target))) {
+		$newPwMsg.addClass("none").removeClass("block");
         return;
     }
 
 	// 비밀번호 4~12자, 영문/숫자/특수문자 중 2개 이상 포함
-	const validCount = /[A-Za-z]/.test(e.target.value)
-						+ /[0-9]/.test(e.target.value)
-						+ /[^A-Za-z0-9]/.test(e.target.value);
+	const validCount = /[A-Za-z]/.test(newPwVal)
+						+ /[0-9]/.test(newPwVal)
+						+ /[^A-Za-z0-9]/.test(newPwVal);
 
-	const passwordLength = e.target.value.length;
-	passwordCheck = passwordLength >= 4
-					&& passwordLength <= 12
-					&& validCount >= 2;
-	
-	newPwMsg.className = passwordCheck ? "none" : "block";
+	const passwordLength = newPwVal.length;
+	const passwordCheck = passwordLength >= 4
+					    && passwordLength <= 12
+					    && validCount >= 2;
+
+    $newPwMsg.toggleClass("block", !passwordCheck).toggleClass("none", passwordCheck);
 	
 });
 
 // 신규 비밀번호
-newPw.addEventListener("change", pwCheck);
+$newPw.on("change", pwCheck);
 
 // 신규 비밀번호 확인
-newPwChk.addEventListener("change", () => {
-    if (empInput(newPw)) newPwChkMsg.className = "block";
-	else pwCheck();
-});
+$newPwChk.on("change", pwCheck);
 
 // 비밀번호 변경
-modifyForm.addEventListener("submit", e => {
+$modifyForm.on("submit", e => {
 
-	const newPwEmpExist = empInput(newPw) || newPwMsg.className === "block";
-	const newPwChkEmpExist = empInput(newPwChk) || newPwChkMsg.className === "block";
+	const newPwEmpExist = empInput($newPw) || $newPwMsg.hasClass("block");
+	const newPwChkEmpExist = empInput($newPwChk) || $newPwChkMsg.hasClass("block");
 
-	if (empInput(originPw)) {
-		originPwMsg.className = "block";
-		prevForm(originPw, e);
+	if (empInput($originPw)) {
+		$originPwMsg.addClass("block").removeClass("none");
+		prevForm($originPw, e);
 	}
-	else if (newPwEmpExist) prevForm(newPw, e);
-	else if (newPwChkEmpExist) prevForm(newPwChk, e);
+	else if (newPwEmpExist) prevForm($newPw, e);
+	else if (newPwChkEmpExist) prevForm($newPwChk, e);
 
 });
 
 // 입력 값 존재 여부 확인 함수
-function empInput(inp) {
-	return inp.value.trim().length === 0;
+function empInput($inp) {
+	return $inp.val().trim().length === 0;
 }
 
 // 비밀번호 일치 여부 확인 함수
 function pwCheck() {
 
-	if (empInput(newPw) || empInput(newPwChk)) {
-		newPwChkMsg.className = "none";
+	if (empInput($newPw) || empInput($newPwChk)) {
+		$newPwChkMsg.addClass("none").removeClass("block");
 		return;
 	}
 	
-	const isValid = newPw.value === newPwChk.value;
-	newPwChkMsg.className = isValid ? "none" : "block";
+	const isValid = $newPw.val() === $newPwChk.val();
+	$newPwChkMsg.toggleClass("block", !isValid).toggleClass("none", isValid);
 
 }
 
 // 페이지가 로드될 때 input 내용 초기화
-window.addEventListener("pageshow", () => {
-	modifyForm.reset();
+$(window).on("pageshow", () => {
+	$modifyForm[0].reset();
 });
 
 // form 태그 제출 중단 함수
-function prevForm(inp, e) {
-	inp.focus();
+function prevForm($inp, e) {
+	$inp.focus();
 	e.preventDefault();
 }
